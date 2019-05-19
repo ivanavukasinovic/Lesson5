36b59beadfbe3a9f9033ac1665eea831c8a5c161
Your Name <you@example.com>
2019-05-19 18:39:32 +0200

    HarryPotter exercise - adding code

--- src/exercises/HarryPotter.java
+++ src/exercises/HarryPotter.java
@@ -27,10 +27,15 @@ public class HarryPotter {
 
 	public static void main(String[] args) {
 		// 1. make harry potter
+		HarryPotter hp = new HarryPotter();
 		// 2. become invisible
+		hp.makeInvisible(true);
 		// 3. spy on professor snape
+		hp.spyOnSnape();
 		// 4. become visible again
-		// 5. cast a “stupefy” spell
+		hp.makeInvisible(false);
+		// 5. cast a stupefy spell
+		hp.castSpell("stupefy");
 	}
 
 }
